6b1fbdca0633c0e5e778f594297d68255302c83b
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-08 16:04:45 +0100

    Add header, app layout and import comic db

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Hello World from main content</h1>
+            </div>
+        </div>
+    </div>
+@endsection
